b5be6c8dc25c675bebacc79820606a40d5c8d157
siddhart <siddhartha.vikram@practicenumbers.com>
2023-06-23 15:42:08 +0530

    Update: API endpoint

--- social_network/urls.py
+++ social_network/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('api.urls')),
+    path('api/', include('api.urls')),
 ]
